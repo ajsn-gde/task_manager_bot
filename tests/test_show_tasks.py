7a7e2588a6aa621ea6cd6fb0f4bfa999c5b78f73
AJSN <ajsn000080@gmail.com>
2026-09-11 09:06:03 +0700

    test(database): add unit test for show all tasks

---
+++ tests/test_show_tasks.py
@@ -0,0 +1,22 @@
+import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import database
+
+@pytest.mark.asyncio
+async def test_get_all_tasks_with_data():
+    # setup db
+    test_engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False)
+    await database.init_db(test_engine)
+    TestSession = async_sessionmaker(test_engine, expire_on_commit=False)
+
+    # add some dummy data
+    await database.add_task_db("Tugas 1", TestSession)
+    await database.add_task_db("Tugas 2", TestSession)
+
+    # get all task
+    tasks = await database.get_all_tasks_db(TestSession)
+
+    # verify the total and description
+    assert len(tasks) == 2
+    assert tasks[0].description == "Tugas 1"
+    assert tasks[1].description == "Tugas 2"
