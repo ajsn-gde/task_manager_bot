6026eccbe60c9c5da8657c8512bf3f180cc03fe6
AJSN <ajsn000080@gmail.com>
2026-09-11 08:59:08 +0700

    test(database): add unit test for deleting not found task

--- tests/test_delete_task.py
+++ tests/test_delete_task.py
@@ -24,3 +24,15 @@ async def test_delete_task_success():
     assert len(tasks) == 0
 
 
+@pytest.mark.asyncio
+async def test_delete_task_not_found():
+    # setup
+    test_engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False)
+    await database.init_db(test_engine)
+    TestSession = async_sessionmaker(test_engine, expire_on_commit=False)
+
+    # remove not found id (ID: 999)
+    is_deleted = await database.delete_task_db(999, TestSession)
+
+    # must be return false because the id is not found in db
+    assert is_deleted is False
